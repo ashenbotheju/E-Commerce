b3a1d690adf05a9ccfa0f6c19152c566584ad877
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-10-07 18:46:56 +0530

    create product screen

--- frontend/src/screens/HomeScreens.js
+++ frontend/src/screens/HomeScreens.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 
-import products from '../products'
+import products from "../products";
 
 function HomeScreens() {
-  return (  
+  return (
     <div>
       <h1>Latest product</h1>
       <Row>
-        {products.map(product => (
-            <Col key={product._id} sm={12} md={6} sl={4} xl={2}>
-                <Product product={product} />
-            </Col>
+        {/* map takes the products array and returns a new array of JSX elements */}
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} sl={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
       </Row>
     </div>
-  )
+  );
 }
 
-export default HomeScreens
+export default HomeScreens;
