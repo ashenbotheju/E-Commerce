018c09c26cd41f08a1bf3a30feed25da8f7c391c
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-10-10 22:46:08 +0530

    start use django rest framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .products import products
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    return Response('Hello')
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProducts(request, pk):
+    return Response(products)
