23548b5725a0f5ce63ab937e7480a6ddfc16288d
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-10-11 09:20:09 +0530

    fetched data

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, Card, Button, ListGroup } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, Card, Button, ListGroup } from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
 
 function ProductScreen() {
-  const { id } = useParams()  // Get the 'id' from the route parameters
-  const product = products.find((p) => p._id == id) // Find the product by the id
+  const { id } = useParams(); // Use useParams hook to get the id from the route
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, []);
+
   return (
     <div>
-      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
@@ -20,19 +32,19 @@ function ProductScreen() {
               <h3>{product.name}</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviws`} color={'#f8e825'} />
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviws`}
+                color={"#f8e825"}
+              />
             </ListGroup.Item>
-            <ListGroup.Item>
-              price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Description: {product.description}
-            </ListGroup.Item>
+            <ListGroup.Item>price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
           <Card>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row>
                   <Col>Price:</Col>
@@ -46,20 +58,26 @@ function ProductScreen() {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Stock'}
+                    {product.countInStock > 0 ? "In Stock" : "Out Stock"}
                   </Col>
                   <Col></Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-black' type='button' disabled={product.countInStock == 0}>Add to Cart</Button>
+                <Button
+                  className="btn-black"
+                  type="button"
+                  disabled={product.countInStock == 0}
+                >
+                  Add to Cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
       </Row>
     </div>
-  )
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
