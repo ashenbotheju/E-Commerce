a227aa261ad191ae9787145ed4769889e89392f4
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-10-11 08:10:07 +0530

    building the backenda

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
-    path('product/<str:pk>/', views.getProducts, name="product")
+    path('products/<str:pk>/', views.getProduct, name="product")
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -16,5 +16,10 @@ def getProducts(request):
     return Response(products)
 
 @api_view(['GET'])
-def getProducts(request, pk):
-    return Response(products)
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
